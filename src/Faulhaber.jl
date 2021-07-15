f2e6c0f4c92b0a0e93b550b7d9892f07b85ad310
Yingbo Ma <mayingbo5@gmail.com>
2021-07-15 00:28:43 -0400

    Add faulhaber

--- src/Faulhaber.jl
+++ src/Faulhaber.jl
@@ -1,5 +1,30 @@
 module Faulhaber
 
-# Write your package code here.
+"""
+    faulhaber(m, ::Val{p})
+
+Compute the summation `sum(i->i^p, 1:m)`. When `m` is an integer, the function
+returns an exact integer solution assuming integer overflows don't occur. When
+`m` is a floating-point number, then it returns a less accurate float solution.
+"""
+@generated function faulhaber(m, ::Val{p}) where p
+    @assert p >= 0
+    T = m
+    IT = typeof(Integer(zero(m)))
+    k = p + 1
+    A = Rational{IT}[m > n ? zero(IT) : (-1)^(m+n) * binomial(IT(n), m-1)  for n in 1:k, m in 1:k]
+    cc = A' \ [i==k for i in 1:k]
+    zz = zero(T)
+    if m <: Integer
+        factor = lcm(denominator.(cc)...)
+        coeffs = T.(factor * cc)
+        :(@evalpoly(m, $zz, $(coeffs...)) ÷ $factor)
+    else
+        coeffs = T.(cc)
+        :(@evalpoly(m, $zz, $(coeffs...)))
+    end
+end
+
+export faulhaber
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,11 @@ using Faulhaber
 using Test
 
 @testset "Faulhaber.jl" begin
-    # Write your tests here.
+    for m in 1:200, p in 1:7
+        @test faulhaber(m, Val(p)) == sum(i->i^p, 1:big(m))
+        @test faulhaber(float(m), Val(p)) ≈ sum(i->i^p, 1:big(m))
+    end
+    for m in 1:13, p in 8:15
+        @test faulhaber(m, Val(p)) == sum(i->i^p, 1:big(m))
+    end
 end
