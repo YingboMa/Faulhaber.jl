b594251294abd5eaec4573b68cb9feb58053de20
Yingbo Ma <mayingbo5@gmail.com>
2021-07-15 00:52:40 -0400

    Make faulhaber work for Int128

--- src/Faulhaber.jl
+++ src/Faulhaber.jl
@@ -7,13 +7,26 @@ Compute the summation `sum(i->i^p, 1:m)` in ``O(p)`` time. When `m` is an
 integer, the function returns an exact integer solution assuming integer
 overflows don't occur. When `m` is a floating-point number, then it returns a
 less accurate float solution.
+
+Examples:
+```julia
+julia> using BenchmarkTools, Faulhaber
+
+julia> @btime faulhaber(Ref(Int128(1000))[], Val(7))
+  1.140 ns (0 allocations: 0 bytes)
+125500583333041666750000
+
+julia> @btime sum(i->i^7, 1:Int128(1000))
+  6.143 μs (0 allocations: 0 bytes)
+125500583333041666750000
+```
 """
 @generated function faulhaber(m, ::Val{p}) where p
     @assert p >= 0
     T = m
     IT = typeof(Integer(zero(m)))
     k = p + 1
-    A = Rational{IT}[m > n ? zero(IT) : (-1)^(m+n) * binomial(IT(n), m-1)  for n in 1:k, m in 1:k]
+    A = Rational{IT}[m > n ? zero(IT) : (-1)^(m+n) * binomial(IT(n), IT(m)-1)  for n in 1:k, m in 1:k]
     cc = A' \ [i==k for i in 1:k]
     zz = zero(T)
     if m <: Integer
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,12 @@ using Faulhaber
 using Test
 
 @testset "Faulhaber.jl" begin
-    for m in 1:200, p in 1:7
-        @test faulhaber(m, Val(p)) == sum(i->i^p, 1:big(m))
-        @test faulhaber(float(m), Val(p)) ≈ sum(i->i^p, 1:big(m))
+    for m in 1:200, p in 1:7, T in [Int, Int128, Float64]
+        if T == Float64
+            @test faulhaber(float(m), Val(p)) ≈ sum(i->i^p, 1:big(m))
+        else
+            @test faulhaber(T(m), Val(p)) === T(sum(i->i^p, 1:big(m)))
+        end
     end
     for m in 1:13, p in 8:15
         @test faulhaber(m, Val(p)) == sum(i->i^p, 1:big(m))
